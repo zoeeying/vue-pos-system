b154f0d7447cf4c2f307087304e704347c4dd1f0
zoeying <zoeeying@gmail.com>
2020-03-05 23:02:58 +0800

    学习了通过事件形式实现子组件向父组件传值

---
+++ src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="child">
+    <button @click="changeNum">点我向父组件传值</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Child',
+    methods: {
+      changeNum() {
+        this.$emit('numChanged', 4869) // 自定义emitChangeNum事件，传值4869
+      }
+    }
+  }
+</script>
+
+
